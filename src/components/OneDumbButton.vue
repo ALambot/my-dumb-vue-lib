5f0d2e072e3c30e58a56f13f15c123514a83457e
ala <ala@n-side.com>
2023-10-10 16:42:48 +0200

    Remove Vue Hello World + Make Dumb Demo App + Integrate Bulma

---
+++ src/components/OneDumbButton.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import {ref} from 'vue'
+
+  // Props definition
+  // They are passed as <OneDumbButton prefix="Your text here" />
+  defineProps({
+    prefix: {
+      required: true
+    }
+  })
+
+  // Reactive proxy to the click count, using ref
+  const count = ref(0)
+
+  // Increment click count
+  // Because we are within the JS script setup, we have to use .value
+  function increment() {
+    count.value += 1
+  }
+</script>
+
+<template>
+  <!-- Within the html template, we can access the value of count directly -->
+  <button class="button m-1 is-primary is-rounded" @click="increment">
+    {{prefix}} ({{count}} clicks)
+  </button>
+</template>
+
+<style>
+</style>
---
+++ src/components/ThreeDumbButtons.vue
@@ -0,0 +1,41 @@
+<script setup>
+  import { computed, ref } from 'vue'
+  import OneDumbButton from './OneDumbButton.vue'
+
+  // Props definition
+  // They are passed as <ThreeDumbButtons direction="vertical" />
+  const props = defineProps({
+    direction: {
+      default: "row"
+    }
+  })
+
+  // Computed properties
+  const directionClass = computed(() => {
+    return `is-flex-direction-${props.direction}`
+  })
+
+  // Data for generating the buttons
+  const buttons = ref([
+    {prefix: "Hi, I'm button 1"},
+    {prefix: "I'm right in the middle"},
+    {prefix: "I don't care !"}
+  ])
+
+</script>
+
+<template>
+  
+  <!-- Bulma CSS classes based on JS (computed) variables -->
+  <div :class="['m-1', 'is-flex', 'is-justify-content-center', directionClass]">
+    
+    <!-- Dynamically generate buttons based on JS variables -->
+    <OneDumbButton
+      v-for="(butt, index) in buttons"
+      :key="index"
+      :prefix="butt.prefix"
+    />
+  
+  </div>
+
+</template>
