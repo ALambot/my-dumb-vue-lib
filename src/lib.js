c884d93921e57d4935c4dc377eb296786b072641
ala <ala@n-side.com>
2023-10-11 12:14:28 +0200

    README

---
+++ src/lib.js
@@ -0,0 +1,9 @@
+// Used to export your components in your library
+
+import OneDumbButton from './components/OneDumbButton.vue'
+import ThreeDumbButtons from './components/ThreeDumbButtons.vue'
+
+export {
+  OneDumbButton,
+  ThreeDumbButtons
+}
